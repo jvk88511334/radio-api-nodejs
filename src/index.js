7653c31554d1da8c12b86b7aec4bce14c225cf6d
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-10-22 17:27:32 +0200

    - Config minimal express pour web server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('le serveur fonctionne')
+// src/index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('service opérationnel sur port ' + PORT);
+});
+
+const PORT = 2000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
